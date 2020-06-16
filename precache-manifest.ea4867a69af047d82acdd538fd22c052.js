273f9487302e94a1068cb6a4f5f30084187329d6
muzeke <sebulinojohnezekiel@gmail.com>
2020-06-16 23:39:00 +0800

    Updates

---
+++ precache-manifest.ea4867a69af047d82acdd538fd22c052.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba50d6d8b76c19d07071ce46cf700427",
+    "url": "/sanctuaryjlc/index.html"
+  },
+  {
+    "revision": "d97ed007e7e2ad368178",
+    "url": "/sanctuaryjlc/static/css/main.0b4cc38d.chunk.css"
+  },
+  {
+    "revision": "2e0406b715a6c08702bf",
+    "url": "/sanctuaryjlc/static/js/2.c57df4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/sanctuaryjlc/static/js/2.c57df4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d97ed007e7e2ad368178",
+    "url": "/sanctuaryjlc/static/js/main.e38c9969.chunk.js"
+  },
+  {
+    "revision": "d6f7121d863d7ad226db",
+    "url": "/sanctuaryjlc/static/js/runtime-main.4637879a.js"
+  },
+  {
+    "revision": "e6e22003c07db60a6bc63d53d2387afe",
+    "url": "/sanctuaryjlc/static/media/logo-V2.e6e22003.png"
+  },
+  {
+    "revision": "b995a89940e3efb5ca6a9d90f235eef4",
+    "url": "/sanctuaryjlc/static/media/logo-solo.b995a899.png"
+  }
+]);
